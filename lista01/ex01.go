dec376fa3ebecf1c5ed9c87af4d9dbf520ce8ccd
luizsaraiva-coder <luizsaraiva@discente.ufg.br>
2026-03-29 02:44:40 -0300

    adicionando exercicios

--- lista01/ex01.go
+++ lista01/ex01.go
@@ -1,1 +1,19 @@
+package main
+import ("fmt")
 
+func main() {
+ var x float32
+ var y float32
+ var z float32
+ var media float32
+ fmt.Scan(&x)
+ fmt.Scan(&y)
+ fmt.Scan(&z)
+ media = (x + y + z) / 3
+fmt.Printf("MEDIA = %.2f\n", media)
+if media>=6 {
+	fmt.Println("APROVADO")
+}else {
+	fmt.Println("REPROVADO")
+}
+}
