a90f0ca403d71b6dfed6d4f0b377aef14976f853
luizsaraiva-coder <luizsaraiva@discente.ufg.br>
2026-04-01 09:10:29 -0300

    adicionando os exercicios

--- lista02/ex05.go
+++ lista02/ex05.go
@@ -1,3 +1,22 @@
 // Escreva um programa que leia um número inteiro e diga se ele é ou não um número divisível por 5.
 
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var n int
+
+	fmt.Println("Informe um valor")
+	fmt.Scan(&n)
+
+	if n%5 == 0 {
+		fmt.Println("Divisível por 5")
+	} else {
+		fmt.Println("Não divisível por 5")
+	}
+
+}
--- lista02/ex06.go
+++ lista02/ex06.go
@@ -2,3 +2,29 @@
 // valores devem ser fornecidos pelo usuário.
 
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var A, B int
+
+	fmt.Println("Digite o valor de A")
+	fmt.Scan(&A)
+
+	fmt.Println("Digite o valor de B")
+	fmt.Scan(&B)
+
+	if B == 0 {
+		fmt.Println("Não é possível verificar divisibilidade por zero")
+		return
+	}
+
+	if A%B == 0 {
+		fmt.Println("A é divisível por B")
+	} else {
+		fmt.Println("A não é divisível por B")
+	}
+}
